712f5db4695105fe1b953225e358e1a51bea6358
Rajat Sharma <sharmarajat687@gmail.com>
2024-04-13 19:42:04 +0530

    Ui responsive

--- src/app/edit/[id]/page.jsx
+++ src/app/edit/[id]/page.jsx
@@ -47,30 +47,28 @@ function page({ params }) {
         <div className='row'>
             <div className='mt-5 col-lg-4'></div>
             <div className='mt-5 col-lg-4 col-md-12' style={{ marginBottom: "25px" }}>
-                <div className='mt-5' style={{ padding: "0px 500px", marginBottom: "25px" }}>
-                    <div className='card' >
-                        <div className='card-header'>
-                            <strong>Add New Todo</strong>
+                <div className='card' >
+                    <div className='card-header'>
+                        <strong>Add New Todo</strong>
+                    </div>
+                    <div className='card-body'>
+                        <div className="mb-3 mt-3">
+                            <label htmlFor="email" className="form-label">Title:</label>
+                            <input type="email" className="form-control" id="email" placeholder="Enter title" name="email" value={title} onChange={(e) => setTitle(e.target.value)} />
                         </div>
-                        <div className='card-body'>
-                            <div className="mb-3 mt-3">
-                                <label htmlFor="email" className="form-label">Title:</label>
-                                <input type="email" className="form-control" id="email" placeholder="Enter title" name="email" value={title} onChange={(e) => setTitle(e.target.value)} />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="pwd" className="form-label">Description:</label>
-                                <textarea name="" id="" className="form-control" cols="30" rows="3" placeholder='Enter Description' value={description} onChange={(e) => setDesc(e.target.value)}></textarea>
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="pwd" className="form-label">Select Status:</label> <br />
-                                <select className='form-control' onChange={(e) => { setStatus(e.target.value) }}>
-                                    <option hidden defaultValue={status} >{status}</option>
-                                    <option value="Complete" >Complete</option>
-                                    <option value="In-Complete">In-Complete</option>
-                                </select>
-                            </div>
-                            <button type="submit" className="btn btn-primary" onClick={submithandler}>Submit</button>
+                        <div className="mb-3">
+                            <label htmlFor="pwd" className="form-label">Description:</label>
+                            <textarea name="" id="" className="form-control" cols="30" rows="3" placeholder='Enter Description' value={description} onChange={(e) => setDesc(e.target.value)}></textarea>
                         </div>
+                        <div className="mb-3">
+                            <label htmlFor="pwd" className="form-label">Select Status:</label> <br />
+                            <select className='form-control' onChange={(e) => { setStatus(e.target.value) }}>
+                                <option hidden defaultValue={status} >{status}</option>
+                                <option value="Complete" >Complete</option>
+                                <option value="In-Complete">In-Complete</option>
+                            </select>
+                        </div>
+                        <button type="submit" className="btn btn-primary" onClick={submithandler}>Submit</button>
                     </div>
                 </div>
             </div>
